8c1ff29db5a90af3f6fd65e757909cbccb63cf27
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-12 11:27:55 +0200

    Add Booking model and bookings controller. Nest  booking routes in flats routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :flats
+  has_many :bookings
 end
---
+++ db/migrate/20170912091549_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_date
+      t.integer :total_price
+      t.references :user, foreign_key: true
+      t.references :flat, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
