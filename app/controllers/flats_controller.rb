3e3b43b990dca36b9d9fe1079b2f2447cf632aa4
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-12 16:20:12 +0200

    fixed flat controller v1
    need more data to continue working on flat controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,11 +1,21 @@
 class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_flat, only: [:show, :edit, :update, :delete]
   def index
+    @flats = Flat.all
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,8 +25,25 @@ class FlatsController < ApplicationController
   end
 
   def update
+     if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  def
+    @flat.destroy!
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:title, :address_line, :postal_code, :city, :country, :pictures, :max_guests, :price, :description)
   end
 end
