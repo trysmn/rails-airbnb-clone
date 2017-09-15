3895d1e2b2f84ccc08b5b4cfe09e87fa24ebc43d
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-15 14:26:36 +0200

    address save method

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -17,10 +17,18 @@ class FlatsController < ApplicationController
     # @flats = Flat.where.not(latitude: nil, longitude: nil)
     @flats = Flat.where("lower(city) LIKE '%' || :city || '%' AND max_guests >= :guests", {city: params['search']['city'].downcase, guests: params['search']['guests']})
 
+    @flats.each do |flat|
+      if flat.latitude.nil?
+        flat.latitude = flat.geocode[0]
+        flat.longitude = flat.geocode[1]
+        flat.save
+      end
+    end
+
     @hash = Gmaps4rails.build_markers(@flats) do |flat, marker|
-    marker.lat flat.latitude
-    marker.lng flat.longitude
-    # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     end
   end
 
